04bf58a0308a5a74ba1b71ca77f22386b93b1814
Guille Dipa <guilledipasquale@gmail.com>
2025-08-18 23:13:20 +1000

    Mas cosas

--- libs/migrator/migrator.go
+++ libs/migrator/migrator.go
@@ -1,3 +1,73 @@
 // Package migrator provides functions to migrate playlists from Spotify to
 // YouTube Music.
 package migrator
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/guilledipa/SpotyToYTM/libs/spotify"
+	"github.com/guilledipa/SpotyToYTM/libs/youtubemusic"
+)
+
+// Migrate reads playlist files from a directory and migrates them to YouTube Music.
+func Migrate(ctx context.Context, ytClient *youtubemusic.Client, playlistsDir string) error {
+	files, err := os.ReadDir(playlistsDir)
+	if err != nil {
+		return fmt.Errorf("could not read playlists directory: %w", err)
+	}
+
+	for _, file := range files {
+		if filepath.Ext(file.Name()) != ".json" {
+			continue
+		}
+
+		log.Printf("Migrating playlist from file: %s", file.Name())
+
+		filePath := filepath.Join(playlistsDir, file.Name())
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Printf("could not read file %s: %v. Skipping.", filePath, err)
+			continue
+		}
+
+		var playlist spotify.Playlist
+		if err := json.Unmarshal(data, &playlist); err != nil {
+			log.Printf("could not unmarshal playlist from file %s: %v. Skipping.", filePath, err)
+			continue
+		}
+
+		// Create a new playlist on YouTube Music.
+		ytPlaylist, err := ytClient.CreatePlaylist(ctx, playlist.Name, "Migrated from Spotify")
+		if err != nil {
+			log.Printf("could not create YouTube Music playlist for %s: %v. Skipping.", playlist.Name, err)
+			continue
+		}
+		log.Printf("Created YouTube Music playlist: %s (ID: %s)", ytPlaylist.Snippet.Title, ytPlaylist.Id)
+
+		// Add tracks to the new playlist.
+		for _, track := range playlist.Tracks {
+			var artists []string
+			for _, artist := range track.Artists {
+				artists = append(artists, artist.Name)
+			}
+			query := fmt.Sprintf("%s %s", track.Name, strings.Join(artists, " "))
+
+			log.Printf("Searching for track: %s", query)
+			_, err := ytClient.SearchAndAddSong(ctx, ytPlaylist.Id, query)
+		if err != nil {
+				log.Printf("could not add track '%s' to playlist '%s': %v", query, ytPlaylist.Snippet.Title, err)
+				// Continue to the next track
+			} else {
+				log.Printf("Successfully added track '%s' to playlist '%s'", query, ytPlaylist.Snippet.Title)
+			}
+		}
+	}
+
+	return nil
+}
--- libs/youtubemusic/youtubemusic.go
+++ libs/youtubemusic/youtubemusic.go
@@ -1,2 +1,153 @@
-// Package youtubemusic provides a client for using the YouTube Music API.
+// Package youtubemusic provides a client for interacting with the YouTube Music API.
 package youtubemusic
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+)
+
+// Client is a wrapper around the YouTube Music client.
+type Client struct {
+	service *youtube.Service
+}
+
+// NewClient creates a new YouTube Music client.
+func NewClient(ctx context.Context, clientSecretFile string) (*Client, error) {
+	b, err := os.ReadFile(clientSecretFile)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, youtube.YoutubeScope)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(ctx, config)
+
+	service, err := youtube.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve YouTube client: %v", err)
+	}
+
+	return &Client{service: service}, nil
+}
+
+// getClient retrieves a token, saves the token, then returns the generated client.
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+// getTokenFromWeb requests a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the " +
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// tokenFromFile retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// saveToken saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+// CreatePlaylist creates a new playlist on YouTube.
+func (c *Client) CreatePlaylist(ctx context.Context, title, description string) (*youtube.Playlist, error) {
+	playlist := &youtube.Playlist{
+		Snippet: &youtube.PlaylistSnippet{
+			Title:       title,
+			Description: description,
+		},
+		Status: &youtube.PlaylistStatus{
+			PrivacyStatus: "private",
+		},
+	}
+
+	call := c.service.Playlists.Insert([]string{"snippet", "status"}, playlist)
+	newPlaylist, err := call.Do()
+	if err != nil {
+		return nil, fmt.Errorf("could not create playlist: %w", err)
+	}
+	return newPlaylist, nil
+}
+
+// SearchAndAddSong searches for a song and adds it to a playlist.
+func (c *Client) SearchAndAddSong(ctx context.Context, playlistID, query string) (*youtube.PlaylistItem, error) {
+	// Search for the song
+	searchCall := c.service.Search.List([]string{"snippet"}).Q(query).MaxResults(1).Type("video")
+	searchResponse, err := searchCall.Do()
+	if err != nil {
+		return nil, fmt.Errorf("could not search for song: %w", err)
+	}
+
+	if len(searchResponse.Items) == 0 {
+		return nil, fmt.Errorf("no results found for query: %s", query)
+	}
+
+	videoID := searchResponse.Items[0].Id.VideoId
+
+	// Add the song to the playlist
+	playlistItem := &youtube.PlaylistItem{
+		Snippet: &youtube.PlaylistItemSnippet{
+			PlaylistId: playlistID,
+			ResourceId: &youtube.ResourceId{
+				Kind:    "youtube#video",
+				VideoId: videoID,
+			},
+		},
+	}
+
+	insertCall := c.service.PlaylistItems.Insert([]string{"snippet"}, playlistItem)
+	insertedItem, err := insertCall.Do()
+	if err != nil {
+		return nil, fmt.Errorf("could not add song to playlist: %w", err)
+	}
+
+	return insertedItem, nil
+}
